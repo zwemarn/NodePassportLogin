e0d57485c6b2195eb5198e40024487faa577ec35
scott_zwe <scott13.zwe@gmail.com>
2019-12-02 18:24:10 +0800

    27mins

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const port = process.env.port || 5000;
+
+app.listen(port, console.log('Server started on port ' + port))
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://admin:admin@testcluster-tet6p.mongodb.net/test?retryWrites=true&w=majority'
+}
